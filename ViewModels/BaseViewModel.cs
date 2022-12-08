0a783da1178b6ffd18c236e32da94e6c830cb69a
lis-dima <lisiev.dmytro@gmail.com>
2022-12-08 22:07:03 +0000

    INotifyPropertyChanged in old style

---
+++ ViewModels/BaseViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maui_CarListApp.ViewModels
+{
+    class BaseViewModel : INotifyPropertyChanged
+    {
+        string title;
+        bool isBusy;
+
+        public bool IsBusy
+        {
+            get => isBusy; set
+            {
+                if (isBusy) return;
+                isBusy = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Title { get => title; set { 
+                
+                if (title == value) return;
+                title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
